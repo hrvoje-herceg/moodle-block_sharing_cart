d2241b459c12c9c44fde9466fea1b3c76aab2561
Ruslan Kabalin <ruslan.kabalin@gmail.com>
2023-09-24 15:58:14 +0100

    Implement warning if subsections depth limit will be exceeded on paste.

--- lang/en/block_sharing_cart.php
+++ lang/en/block_sharing_cart.php
@@ -65,6 +65,7 @@ $string['copy_activity'] = 'Copy activity';
 $string['copy_activity_title'] = 'Copy selected activity';
 $string['copy_activity_reason'] = 'This view is available because you have access to backup activities, but no access to manage/move activities.';
 $string['label_image_replaced_text'] = ' (Label: Image)';
+$string['subsectiondepthwarning'] = 'Requested subsections hierarchy can\'t be created, as it will produce subsections depth of {$a->depth}, which exceeds course defined subsection depth limit of {$a->depthlimit}.';
 $string['uninstalled_plugin_warning_title'] = 'This plugin is uninstalled. Trying to recover this without reinstalling the plugin: {$a} will cause errors. Restoration is disabled';
 $string['drop_here'] = 'Drop here..';
 $string['async_restore_in_progress'] = 'Restoring sharing item "{$a->modtext}" to the section number {$a->section}';
