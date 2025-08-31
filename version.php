00b1602b670ea821f82f75b679a0adc27e42d089
Ruslan Kabalin <ruslan.kabalin@gmail.com>
2025-08-31 19:55:07 +0100

    Bump version and set Moodle 4.4 requirement.

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die;
 
 /** @var object $plugin */
 $plugin->component = 'block_sharing_cart';
-$plugin->version   = 2024022700;
-$plugin->requires  = 2023042400; // Moodle 4.2.0
+$plugin->version   = 2025083100;
+$plugin->requires  = 2024042200; // Moodle 4.4
 $plugin->release   = '4.4, release 4';
 $plugin->maturity  = MATURITY_STABLE;
